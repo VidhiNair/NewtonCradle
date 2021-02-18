4a338584b0707002f9d7d9f67d14abe886269a03
Vidhi Nair <74731487+VidhiNair@users.noreply.github.com>
2021-02-18 17:08:55 +0000

    Add files via upload

---
+++ String.js
@@ -0,0 +1,25 @@
+class String{
+    constructor(bodyA, pointB){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointB,
+            stiffness: 1,
+            length: 200,
+            damping: 0
+        }
+        this.bodyA = bodyA;
+        this.pointB = pointB;
+        this.sling = Constraint.create(options);
+        Matter.World.add(world, this.sling);
+
+    }
+
+    display(){
+         var pointA=this.sling.bodyA.position;
+         var pointB=this.pointB
+         push();
+
+         line(pointA.x - 20, pointA.y, pointB.x-10, point.y);
+    }
+
+}
